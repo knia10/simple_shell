fa63521c441b031af7facc7db99743de314542e4
knia10 <kelly.stephania@hotmail.com>
2021-08-25 01:08:07 -0500

    Add file exit

---
+++ _exit.c
@@ -0,0 +1,50 @@
+#include "shell.h"
+
+/**
+ * c_atoi - converts the string into an integer
+ * @str: string
+ * Return: number if success, -1 if string contains non-numbers
+ */
+int c_atoi(char *str)
+{
+	int i = 0;
+	unsigned int num = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 48 && str[i] <= 57) /* calculate num */
+			num = num * 10 + (str[i] - 48);
+		if (str[i] > 57 || str[i] < 48) /* account for non-numbers */
+			return (-1);
+		i++;
+	}
+	return (num);
+}
+
+/**
+ * f_exit - cause process termination
+ * @str: user's command into shell
+ * @env: environmental variable
+ * @num: int
+ * @lineptr: the command
+ * Return: 0 if success 2 if fail
+ */
+int f_exit(char **str, list_t *env, int num, char **lineptr)
+{
+	int value = 0;
+
+	if (str[1] != NULL)
+		value = c_atoi(str[1]);
+
+	if (value == -1)
+	{
+		illegal_number(str[1], num, env); /* imprime mensaje de error */
+		free_pptr(str);
+		return (2);
+	}
+	free_pptr(str); /* free user input before exiting program */
+	free_linked_list(env);
+	if (lineptr != NULL)
+		free_pptr(lineptr);
+	exit(value);
+}
